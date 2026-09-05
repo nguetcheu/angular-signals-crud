aa9b4b66d5cc9a2c83db9e6471d11e74c72f9137
nguetcheu <dnguetcheu@gmail.com>
2026-09-06 01:52:41 +0200

    Ajout des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,30 @@
 import { Routes } from '@angular/router';
 import { PersonsListComponent } from './components/persons-list/persons-list.component';
+import { PersonFormComponent } from './components/person-form/person-form.component';
 
 export const routes: Routes = [
-  { path: '', component: PersonsListComponent, pathMatch: 'full' },
+  // Route principale : affiche la liste des personnes
+  {
+    path: '',
+    component: PersonsListComponent,
+    pathMatch: 'full',
+  },
+
+  // Route de création : affiche le formulaire vierge
+  {
+    path: 'add',
+    component: PersonFormComponent,
+  },
+
+  // Route d'édition : réutilise le formulaire pré-rempli grâce à l'ID
+  {
+    path: 'edit/:id',
+    component: PersonFormComponent,
+  },
+
+  // Redirection automatique en cas d'URL inconnue
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
